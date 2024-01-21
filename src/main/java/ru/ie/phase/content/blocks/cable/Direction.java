e1f1cb865dc40b27c3a4ffb73f7a1757441c687f
ienumerable <thepashalza@gmail.com>
2024-01-22 00:39:59 +0600

    Добавление: invert() позволяет инвертировать направление

--- src/main/java/ru/ie/phase/content/blocks/cable/Direction.java
+++ src/main/java/ru/ie/phase/content/blocks/cable/Direction.java
@@ -1,10 +1,43 @@
 package ru.ie.phase.content.blocks.cable;
 
 public enum Direction {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST,
-    UP,
-    DOWN
+    NORTH {
+        @Override
+        public Direction invert() {
+            return WEST;
+        }
+    },
+    SOUTH {
+        @Override
+        public Direction invert() {
+            return EAST;
+        }
+    },
+    WEST {
+        @Override
+        public Direction invert() {
+            return NORTH;
+        }
+    },
+    EAST {
+        @Override
+        public Direction invert() {
+            return SOUTH;
+        }
+    },
+    UP {
+        @Override
+        public Direction invert() {
+            return DOWN;
+        }
+    },
+    DOWN {
+        @Override
+        public Direction invert() {
+            return UP;
+        }
+    };
+
+    public abstract Direction invert();
+
 }
