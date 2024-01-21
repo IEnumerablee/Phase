b84f6892ca7a60580cdc06ad76c6a699cd432921
ienumerable <thepashalza@gmail.com>
2024-01-22 03:02:40 +0600

    Добавление: метод getDir, методы check для элементов
    check проверяет, находится ли p2 относительно p1 по этой стороне света
    
    getDir находит сторону нахождения p2 относительно p1

--- src/main/java/ru/ie/phase/content/blocks/cable/Direction.java
+++ src/main/java/ru/ie/phase/content/blocks/cable/Direction.java
@@ -1,28 +1,50 @@
 package ru.ie.phase.content.blocks.cable;
 
+import net.minecraft.core.BlockPos;
+
 public enum Direction {
     NORTH {
         @Override
         public Direction invert() {
-            return WEST;
+            return SOUTH;
+        }
+
+        @Override
+        public boolean check(BlockPos p1, BlockPos p2) {
+            return p1.getZ() > p2.getZ();
         }
     },
     SOUTH {
         @Override
         public Direction invert() {
-            return EAST;
+            return NORTH;
+        }
+
+        @Override
+        public boolean check(BlockPos p1, BlockPos p2) {
+            return p1.getZ() < p2.getZ();
         }
     },
     WEST {
         @Override
         public Direction invert() {
-            return NORTH;
+            return EAST;
+        }
+
+        @Override
+        public boolean check(BlockPos p1, BlockPos p2) {
+            return p1.getX() > p2.getX();
         }
     },
     EAST {
         @Override
         public Direction invert() {
-            return SOUTH;
+            return WEST;
+        }
+
+        @Override
+        public boolean check(BlockPos p1, BlockPos p2) {
+            return p1.getX() < p2.getX();
         }
     },
     UP {
@@ -30,14 +52,32 @@ public enum Direction {
         public Direction invert() {
             return DOWN;
         }
+
+        @Override
+        public boolean check(BlockPos p1, BlockPos p2) {
+            return p1.getY() < p2.getY();
+        }
     },
     DOWN {
         @Override
         public Direction invert() {
             return UP;
         }
+
+        @Override
+        public boolean check(BlockPos p1, BlockPos p2) {
+            return p1.getY() > p2.getY();
+        }
     };
 
     public abstract Direction invert();
+
+    public abstract boolean check(BlockPos p1, BlockPos p2);
+
+    public static Direction getDir(BlockPos p1, BlockPos p2){
+        for(Direction d : Direction.values())
+            if(d.check(p1, p2)) return d;
+        throw new IllegalArgumentException("Positions cannot be the equal");
+    }
 
 }
