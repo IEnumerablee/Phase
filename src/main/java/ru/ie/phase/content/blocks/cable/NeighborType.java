1bd1092af34837aa444af15fa8bc876fbff34fc4
ienumerable <thepashalza@gmail.com>
2024-01-24 20:52:17 +0600

    Добавление: енам для описания блока-соседа

---
+++ src/main/java/ru/ie/phase/content/blocks/cable/NeighborType.java
@@ -0,0 +1,28 @@
+package ru.ie.phase.content.blocks.cable;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import ru.ie.phase.foundation.net.ICable;
+import ru.ie.phase.foundation.net.NetNode;
+
+import java.util.function.BiPredicate;
+
+public enum NeighborType {
+
+    NONE((block, blockEntity) -> blockEntity == null && (block == Blocks.AIR || block == Blocks.CAVE_AIR || block == Blocks.VOID_AIR)),
+    CABLE((block, blockEntity) -> blockEntity instanceof ICable),
+    NODE((block, blockEntity) -> blockEntity instanceof NetNode),
+    BLOCK((block, blockEntity) -> block != Blocks.AIR && block != Blocks.CAVE_AIR && block != Blocks.VOID_AIR);
+
+    private final BiPredicate<Block, BlockEntity> checker;
+
+    NeighborType(BiPredicate<Block, BlockEntity> checker){
+        this.checker = checker;
+    }
+
+    public boolean check(Block block, BlockEntity blockEntity){
+        return checker.test(block, blockEntity);
+    }
+
+}
